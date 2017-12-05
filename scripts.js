638ffff60839b79b46c29ce9b2131833e08594d9
Magda Balun <magi@op.pl>
2017-12-05 09:01:46 +0100

    Change .js file

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 var a = 6,
 	h = 20,
-	triangle1Area = getTriangleArea(10, 15);
-	triangle2Area = getTriangleArea(4, 0);
+	triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(4, 0),
 	triangle3Area = getTriangleArea(12, -6);
 function getTriangleArea(a, h) {
 	if ((a > 0) && (h > 0)) {
 		return a * h / 2;
 	}
 	else {
-		return ("Incorrect data!");
+		return "Incorrect data!";
 	}
 }
 console.log(getTriangleArea(a, h));
